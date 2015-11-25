6a55328ed25a5c4cbf24090b07aef0aabb2f3dcc
vbushuev <v.bushuev.co@gmail.com>
2015-11-25 14:54:58 +0300

    Файлы

---
+++ business/bg/index.php
@@ -0,0 +1,145 @@
+<div class="pageTitle fs24 text-white">
+    <div class="inner">
+        Банковская гарантия
+    </div>
+</div>
+<section class="promoBlock promoBlockInfo noPadding" style="background-image: url('<?=SITE_TEMPLATE_PATH?>/images/rko-prom.jpg');">
+    <div class="bgTransparentBlue">
+        <div class="inner text-white align-c">
+            <h2 class="margin-bottom-m">Получите банковскую гарантию прямо сейчас</h2>
+            <div class="tabsSwitcherContainer">
+                <div class="btnsSwitch">
+                    <ul class="menuItems tabsSwitcher">
+                        <li><a href="#switch_0" class="btn btn_sec_two active">Через интернет</a></li>
+                        <li><a href="#switch_1" class="btn btn_sec_two">В офисе</a></li>
+                    </ul>
+                </div>
+                <div id="switch_0" class="switchable active">
+                    <p>Зарегистрируйте заявку в интернете самостоятельно всего за 5 минут</p>
+                    <svg class="ico-svg" data-ico="monitor"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="<?=SITE_TEMPLATE_PATH?>/images/icon-set.svg#monitor"></use></svg>
+                    <ul class="row customItems">
+                        <li class="col4">
+                            <div class="itemNumb">1</div>
+                            <h4>Зарезервируйте номер счета</h4>
+                            <p>Зарезервированный номер счета можно указывать в договорах, заключаемых
+с контрагентами, так как он не изменится после открытия счета.</p>
+                        </li>
+                        <li class="col4">
+                            <div class="itemNumb">2</div>
+                            <h4>Загрузите документы</h4>
+                            <p>Документы будут проверены
+без Вашего личного присутствия
+в Банке с целью экономии
+Вашего времени.</p>
+                        </li>
+                        <li class="col4">
+                            <div class="itemNumb">3</div>
+                            <h4>Доставьте документы</h4>
+                            <p>Курьер бесплатно заберет документы
+в удобном для вас месте.</p>
+                        </li>
+                    </ul>
+                    <a href="#formRKO" class="btn btn-imp openPopUp">ОФОРМИТЬ ЗАЯВКУ</a>
+                </div>
+                <div id="switch_1" class="switchable">
+                    <p>Посетите ближайший офис банка, где наши менеджеры помогут найти оптимальное <br>
+решение для вашего бизнеса</p>
+                    <svg class="ico-svg" data-ico="locate_big"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="<?=SITE_TEMPLATE_PATH?>/images/icon-set.svg#locate_big"></use></svg>
+                    <div>
+                        <svg class="ico-svg" data-ico="check"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="<?=SITE_TEMPLATE_PATH?>/images/icon-set.svg#check"></use></svg>
+                    </div>
+                    <div class="row">
+                        <div class="col8 offcol2">
+                            <h4>Услуга «Срочное открытие счета за 3 часа»</h4>
+                            <p>Предоставляется при наличии требуемого пакета документов, при прохождение всех проверок. Стоимость услуги – 3 500 рублей.</p>
+                        </div>
+                    </div>
+                    <a href="#formBG" class="btn btn-imp openPopUp">ОФОРМИТЬ ЗАЯВКУ</a>
+                </div>
+            </div><!-- .tabsSwitcherContainer -->
+
+        </div>
+    </div>
+</section>
+<section>
+    <div class="inner">
+        <h3>Может пригодится</h3>
+        <ul class="documentLinks">
+            <li><a href="#">Тарифы на РКО для  РФ и иностранной валюте действуют с 01 сентября 2015 года</a></li>
+            <li><a href="#">Тарифы на РКО для юрлиц и ИП без образования ЮЛ в валюте РФ и иностранной валюте действуют с 01 сентября 2015 года</a></li>
+            <li><a href="#">года</a></li>
+            <li><a href="#">и иностранной валюте действуют с 01 сентября 2015 года</a></li>
+        </ul>
+
+        <h3>Типовые формы</h3>
+        <ul class="documentLinks">
+            <li>
+                <a href="#">Заявление на открытие счета (Приложение №1)</a><br>
+                <a href="#">Договор РКО</a><br>
+                <a href="#">Перечень документов для Резидента РФ (Приложение №3)</a><br>
+                <a href="#">Перечень документов для Нерезиденту РФ (Приложение №4)</a><br>
+                <a href="#">Анкета Клиента (Приложение №7)</a><br>
+                <a href="#">Перечень документов для открытия накопительного (временного) счета (Приложение №6)</a><br>
+            </li>
+        </ul>
+        <div class="tabsSwitcherContainer">
+            <div class="align-c docsSwitcher">
+                <h5><strong>Для ИП и лиц, занимающихся частной практикой</strong></h5>
+                <ul class="menuItems tabsSwitcher margin-bottom-m">
+                    <li><a href="#docs_item_0" class="btn btn_grey active">Валюта РФ</a></li>
+                    <li><a href="#docs_item_1" class="btn btn_grey">Иностранная валюта</a></li>
+                </ul>
+            </div>
+
+            <div class="switchable active" id="docs_item_0">
+                <ul class="documentLinks">
+                    <li>
+                        <a href="#">Заявление на открытие счета (Приложение №1)</a><br>
+                        <a href="#">Договор РКО</a><br>
+                        <a href="#">Перечень документов для Резидента РФ (Приложение №3)</a><br>
+                        <a href="#">Перечень документов для Нерезиденту РФ (Приложение №4)</a><br>
+                        <a href="#">Анкета Клиента (Приложение №7)</a><br>
+                        <a href="#">Перечень документов для открытия накопительного (временного) счета (Приложение №6)</a><br>
+                    </li>
+                </ul>
+            </div>
+            <div class="switchable" id="docs_item_1">
+                <ul class="documentLinks">
+                    <li>
+                        <a href="#">Договор РКО</a><br>
+                        <a href="#">Заявление на открытие счета (Приложение №1)</a><br>
+                        <a href="#">Перечень документов для Резидента РФ (Приложение №3)</a><br>
+                        <a href="#">Перечень документов для Нерезиденту РФ (Приложение №4)</a><br>
+                        <a href="#">Перечень документов для открытия накопительного (временного) счета (Приложение №6)</a><br>
+                        <a href="#">Анкета Клиента (Приложение №7)</a><br>
+                    </li>
+                </ul>
+            </div>
+        </div><!-- .tabsSwitcherContainer -->
+
+        <hr>
+
+        <p class="text-grey-light">
+            Документы, представляемые в Банк для открытия счета, должны быть подписаны уполномоченным лицом. Подпись уполномоченного лица, представляющего юридической лицо, должна быть заверена печатью этого юридического лица.
+В документах, представляемых в Банк и содержащих более одного листа, листы должны быть пронумерованы, прошиты
+и скреплены на оборотной стороне последнего листа заверительной надписью с указанием цифрами и прописью количества пронумерованных листов, подписанной ее составителем с указанием фамилии, имени, отчества (если последнее имеется), указанием должности и даты составления.
+        </p>
+    </div><!-- .inner -->
+</section>
+<section class="bgGray">
+    <div class="inner align-c learnMore fs24">
+        <?$APPLICATION->IncludeComponent(
+			"bitrix:main.include",
+			".default",
+			array(
+				"AREA_FILE_SHOW" => "file",
+				"AREA_FILE_SUFFIX" => "inc",
+				"AREA_FILE_RECURSIVE" => "N",
+				"EDIT_TEMPLATE" => "",
+				"COMPONENT_TEMPLATE" => ".default",
+				"PATH" => "/include/telephone.php"
+			),
+			false
+		);?>
+    </div>
+</section>
